8ab4e93a303fbffa509c046b260113def1d9d59c
baby7305 <songhaibei7305@163.com>
2019-07-16 15:43:45 +0800

    配置udp client

--- src/main/java/com/example/socket/udp/UdpEchoServer.java
+++ src/main/java/com/example/socket/udp/UdpEchoServer.java
@@ -44,6 +44,8 @@ public class UdpEchoServer {
 				UdpClient.create()               // Prepares a UDP client for configuration.
 						.port(server.address()  // Obtains the server's port and provide it as a port to which this
 								.getPort()) // client should connect.
+						.runOn(loopResources)   // Configures the UDP client to run on a specific LoopResources.
+						.wiretap(true)              // Applies a wire logger configuration.
 						.connect()              // Connects the client.
 						.block();               // Subscribes to the returned Mono<Connection> and block.
 
