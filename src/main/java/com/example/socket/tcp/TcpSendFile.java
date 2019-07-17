1ae01946b572237c9f2189d5ef467dccd01ec130
baby7305 <songhaibei7305@163.com>
2019-07-17 16:05:04 +0800

    tcp client添加io handler

--- src/main/java/com/example/socket/tcp/TcpSendFile.java
+++ src/main/java/com/example/socket/tcp/TcpSendFile.java
@@ -10,10 +10,14 @@ import reactor.netty.tcp.TcpClient;
 import reactor.netty.tcp.TcpServer;
 
 import javax.net.ssl.SSLException;
+import java.io.IOException;
 import java.net.URISyntaxException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.cert.CertificateException;
+import java.util.Arrays;
+import java.util.concurrent.CountDownLatch;
 
 public class TcpSendFile {
 	public static void main(String[] args) throws CertificateException, SSLException {
@@ -36,12 +40,26 @@ public class TcpSendFile {
 						}).log("tcp-server"))
 						.bindNow(); // Starts the server in a blocking fashion, and waits for it to finish initializing.
 
+		CountDownLatch latch = new CountDownLatch(1);
+		
 		Connection client = TcpClient.create() // Prepares a TCP client for configuration.
 				.port(server.port()) // Obtains the server's port and provide it as a port to which this
 										// client should connect.
 				// Configures SSL providing an already configured SslContext.
 				.secure(SslContextBuilder.forClient().trustManager(InsecureTrustManagerFactory.INSTANCE).build())
 				.wiretap(true) // Applies a wire logger configuration.
+				.handle((in, out) -> out.sendString(Mono.just("/index.html"))
+						.then(in.receive().asByteArray().doOnNext(actualBytes -> {
+							try {
+								Path file = Paths.get(TcpSendFile.class.getResource("/index.html").toURI());
+								byte[] expectedBytes = Files.readAllBytes(file);
+								if (Arrays.equals(expectedBytes, actualBytes)) {
+									latch.countDown();
+								}
+							} catch (URISyntaxException | IOException e) {
+								e.printStackTrace();
+							}
+						}).log("tcp-client").then()))
 				.connectNow(); // Blocks the client and returns a Connection.
 
 		server.disposeNow(); // Stops the server and releases the resources.
