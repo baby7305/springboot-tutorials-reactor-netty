b0c460b1b9c9ad31832de929243248077189c795
baby7305 <songhaibei7305@163.com>
2019-07-22 13:25:06 +0800

    结合线程池Executor、多线程Runnable实现通信
    Server在serverSocket、线程池Executor pool下面、socket外部套一层死循环。新建一个线程类ReadRunnable，new出对象放入线程池pool执行，实现长连接。
    客户端部分和前面共用了BeginClient。

---
+++ src/main/java/com/example/socket/tcp/threadSocket/Server.java
@@ -0,0 +1,37 @@
+package com.example.socket.tcp.threadSocket;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class Server {
+	private ServerSocket serverSocket;
+	private Executor pool;
+
+	public Server(int port, int poolSize) {
+		try {
+			serverSocket = new ServerSocket(port);
+			pool = Executors.newFixedThreadPool(poolSize);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void startService() {
+		try {
+			for (; ; ) {
+				Socket socket = serverSocket.accept();
+				pool.execute(new ReadRunnable(socket));
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		Server server = new Server(8888, 10000);
+		server.startService();
+	}
+}
